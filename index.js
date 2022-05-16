c0e8f0143241ed8f386e2df64f371903544d7ee1
poposki317 <philipedekobi@gmail.com>
2022-05-16 09:07:25 +0100

    set up server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const http = require('http');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+}));
+
+async(() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        const server = await http.createServer(app);
+        await server.listen(PORT, () => console.log(`listening on port: ${port}`));
+    } catch(error){
+        if(error){
+            throw error;
+        } 
+    }
+})();
