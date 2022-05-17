edde102c7c02bc32083fb9820f49bd3bb6cbb2de
poposki317 <philipedekobi@gmail.com>
2022-05-17 14:34:04 +0100

    completed the frontend

--- client_/src/App.js
+++ client_/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, useMediaQuery } from '@chakra-ui/react';
 import { Form, ResultsPage } from './components';
 
 function App() {
+  const [isSmall] = useMediaQuery("(max-width: 600px)")
   const [isLoading, setIsLoading] = React.useState(false);
 
   return (
     <Flex minW="100vw" maxW="100vw" minH="100vh" >
-      <Flex minW="35%" maxW="35%">
+      <Flex minW={isSmall ? "100%" : "35%"} maxW={isSmall ? "100%" : "35%"} h={isSmall ? "45vh" : ""}>
         <Form setIsLoading={setIsLoading} />
       </Flex>
 
-      <Flex minW="65%" maxW="65%">
+      <Flex minW={isSmall ? "100%" : "65%"} maxW={isSmall ? "100%" : "65%"}>
         <ResultsPage isLoading={isLoading} />
       </Flex>
     </Flex>
